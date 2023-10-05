bebfe936e14457952df9f92300672f5f0daac19d
Lenayah <lfanesie@gmail.com>
2023-10-05 13:40:48 -0400

    Upodate

--- Lab - Classes.playground/Pages/4. Exercise - Class Memberwise Initializers and References.xcplaygroundpage/Contents.swift
+++ Lab - Classes.playground/Pages/4. Exercise - Class Memberwise Initializers and References.xcplaygroundpage/Contents.swift
@@ -4,14 +4,18 @@
  - Note: The exercises below are based on a game where a spaceship avoids obstacles in space. The ship is positioned at the bottom of a coordinate system and can only move left and right while obstacles "fall" from top to bottom. The base class `Spaceship` and subclasses `Fighter` and `ShieldedShip` have been provided for you below. You will use these to complete the exercises.
  */
 class Spaceship {
-    let name: String
+    var name: String
     var health: Int
     var position: Int
 
     func moveLeft() {
         position -= 1
     }
-
+    init(name: String, health: Int, position: Int){
+        self.name = name
+        self.health = health
+        self.position = position
+    }
     func moveRight() {
         position += 1
     }
@@ -28,18 +32,32 @@ class Fighter: Spaceship {
     let weapon: String
     var remainingFirePower: Int
 
+    init(weapon: String, remainingFirePower: Int, name:String, health:Int, position: Int) {
+        self.weapon = weapon
+        self.remainingFirePower = remainingFirePower
+        super.init(name: name, health: health, position: position)
+        
+    }
     func fire() {
-        if remainingFirePower > 0 {
+        if (remainingFirePower > 0) {
             remainingFirePower -= 1
-        } else {
+        }
+        else {
             print("You have no more fire power.")
         }
     }
 }
+    
+
 
 class ShieldedShip: Fighter {
     var shieldStrength: Int
 
+    init(shieldStrength: Int, weapon:String, remainingFirePower: Int, name: String, health: Int, position: Int) {
+        self.shieldStrength = shieldStrength
+        super.init(weapon: weapon, remainingFirePower: remainingFirePower, name: name, health: health, position: position)
+        
+    }
     override func wasHit() {
         if shieldStrength > 0 {
             shieldStrength -= 5
@@ -53,22 +71,25 @@ class ShieldedShip: Fighter {
 
  Then create an instance of `Spaceship` below called `falcon`. Use the memberwise initializer you just created. The ship's name should be "Falcon."
  */
-
+let falcon = Spaceship(name: "Falcon", health: 0, position: 0)
+print(falcon.name)
 
 /*:
  Writing initializers for subclasses can get tricky. Your initializer needs to not only set the properties declared on the subclass, but also set all of the uninitialized properties on classes that it inherits from. Go to the declaration of `Fighter` and write an initializer that takes an argument for each property on `Fighter` and for each property on `Spaceship`. Set the properties accordingly. (Hint: you can call through to a superclass's initializer with `super.init` *after* you initialize all of the properties on the subclass).
 
  Then create an instance of `Fighter` below called `destroyer`. Use the memberwise initializer you just created. The ship's name should be "Destroyer."
  */
+let destroyer = Fighter(weapon: "", remainingFirePower: 0, name: "Destroyer", health: 0, position: 0)
 
-
+print(destroyer.name)
 /*:
  Now go add an initializer to `ShieldedShip` that takes an argument for each property on `ShieldedShip`, `Fighter`, and `Spaceship`, and sets the properties accordingly. Remember that you can call through to the initializer on `Fighter` using `super.init`.
 
  Then create an instance of `ShieldedShip` below called `defender`. Use the memberwise initializer you just created. The ship's name should be "Defender."
  */
+let defender = ShieldedShip(shieldStrength: 0, weapon: "", remainingFirePower: 0, name: "Defender", health: 0, position: 0)
 
-
+print(defender.name)
 //:  Create a new constant named `sameShip` and set it equal to `falcon`. Print out the position of `sameShip` and `falcon`, then call `moveLeft()` on `sameShip` and print out the position of `sameShip` and `falcon` again. Did both positions change? Why? If both were structs instead of classes, would it be the same? Why or why not? Provide your answer in a comment or print statement below.
 
 
